8e4e713ad8f3e3789f7f0513be62a55198e7228e
mg901 <mgreen097@gmail.com>
2019-02-13 13:08:20 +0300

    chore: make node-list component

---
+++ src/components/node-list.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { createComponent } from 'effector-react';
+import { nodes } from '../stores';
+import { Node } from './node';
+
+export const NodeList = createComponent(nodes, ({ deleteNode }, list) => (
+  <ul>
+    {list.map(({ id, text }) => (
+      <Node deleteNode={deleteNode} description={text} id={id} key={id} />
+    ))}
+  </ul>
+));
