17b38c133ab4f5dbbf230cdb926b1e0ed50e2c27
mg901 <mgreen097@gmail.com>
2019-02-13 13:07:54 +0300

    chore(components): meke index file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { AddNode } from './add-node';
+export { AddNodeForm } from './add-node-form';
+export { FormInput } from './form-input';
+export { NodeList } from './node-list';
+export { Node } from './node';
