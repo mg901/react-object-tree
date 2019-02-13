d374ce031c3abe8a598fa4413ce844aec055b3de
mg901 <mgreen097@gmail.com>
2019-02-13 13:05:29 +0300

    refactor(app): remove hot-loader for root-reducer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { rootReducer } from './store/root-reducer';
-import { store } from './store/configure-store';
 import { App } from './app';
 
 const render = () => {
@@ -21,10 +19,5 @@ if (module.hot) {
   // Reload components
   module.hot.accept('./app', () => {
     render();
-  });
-
-  // Reload reducers
-  module.hot.accept('./store/root-reducer', () => {
-    store.replaceReducer(rootReducer);
   });
 }
