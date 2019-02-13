831974d9a400603fac2ac9dcb2e68955dad68330
mg901 <mgreen097@gmail.com>
2019-02-13 13:02:42 +0300

    chore: add events

---
+++ src/events.js
@@ -0,0 +1,5 @@
+import { createEvent } from 'effector';
+
+export const changeField = createEvent('change field');
+export const addNode = createEvent('add node');
+export const deleteNode = createEvent('delete node');
