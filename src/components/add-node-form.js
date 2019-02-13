5514817294637b35b73f595f8855dada2fc4ed94
mg901 <mgreen097@gmail.com>
2019-02-13 13:07:02 +0300

    chore: make add-node-form component

---
+++ src/components/add-node-form.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { createComponent } from 'effector-react';
+import { inputText } from '../stores';
+
+export const AddNodeForm = createComponent(
+  inputText,
+  ({ addNode, children }, text) => (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (text.length > 0) {
+          addNode(text.trim());
+        }
+      }}
+    >
+      {children}
+    </form>
+  ),
+);
